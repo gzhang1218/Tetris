c65977467a98c08b8664053e4066e4dda676cc9f
George Zhang <gzhang1218@gmail.com>
2017-10-13 16:12:58 -0500

    commented TacticalBrain

--- src/assignment/TacticalBrain.java
+++ src/assignment/TacticalBrain.java
@@ -12,14 +12,9 @@ public class TacticalBrain implements Brain {
 
     private Random rand = new Random();
 
+    //brain is constructed using the given weights
     public TacticalBrain() {
-        //weights = new double[]{-0.6102501489040298, 0.714374500112446, -0.9015603583671786, -0.4715603583671786, -0.52544587956874866};
-        //weights = new double[]{-1.020828988488132, 0.9292577207656729, -1.3586823994937924, -0.20814941564555897, -0.6152158288454252, 0.11026280385014185, -0.3805911191245738}; //goodone
-        //weights = new double[]{-1.3067101996001897, -0.5776263525445752, -1.3240709146729546, -0.31311290018089394, -0.7883146132012633, -0.29163742419386496, -0.6307497961281868};
-        //weights = new double[]{-0.510066, 0.760666, -0.35663, 0, -0.184483, 0, 0};
-        //weights = new double[]{-1.5406684363464582, -0.7509873596334373, -1.4781405702056236, -0.3194835404635906, -0.6745877009088455, 0.01867967152179941, -0.565265239315107};
-        weights = new double[]{-1.020828988488132, 0.9292577207656729, -1.3586823994937922, -0.11175037697890922, -0.38568284549465454, 0.12050772756889917, 0}; //bestone
-        //genRandomWeights();
+        weights = new double[]{-1.020828988488132, 0.9292577207656729, -1.5586823994937922, -0.11175037697890922, -0.38568284549465454, 0.12050772756889917, 0};
     }
 
     public double[] getWeights(){
@@ -40,7 +35,7 @@ public class TacticalBrain implements Brain {
      * Decide what the next move should be based on the state of the board.
      */
     public Board.Action nextMove(Board currentBoard) {
-        // Fill the our options array with versions of the new Board
+        // Fill our options array with all possible placements given the current Piece and Board.
         options = new ArrayList<>();
         firstMoves = new ArrayList<>();
         enumerateOptions(currentBoard);
@@ -57,7 +52,8 @@ public class TacticalBrain implements Brain {
             }
         }
 
-/*        System.out.println("I chose the " + bestIndex + "th move out of " + options.size());
+        //DEBUGGING
+/*      System.out.println("I chose the " + bestIndex + "th move out of " + options.size());
 
         System.out.println("I'm " + firstMoves.get(bestIndex));
 
@@ -68,9 +64,9 @@ public class TacticalBrain implements Brain {
     }
 
     /**
-     * Test all drops with rotations
+     * Gets all possible orientations and placements of the current piece on the board.
      *
-     * UPDATE: needed to rotate before translating -- I pieces were not working!
+     * Gets first move needed to get to a particular placement.
      */
     void enumerateOptions(Board currentBoard) {
 
@@ -184,24 +180,27 @@ public class TacticalBrain implements Brain {
 
     }
 
+    /**
+     * Assigns a score for a Board state based on weighted parameters.
+     *
+     * Let w# represent the corresponding weight
+     * Scoring function: score = w0*totalHeight + w1*fullRows + w2*holes + w3*blockades + w4*inconsistency + w5*edgeBlocks + w6*floorBlocks
+     *
+     * Maximize (positive weights ideally): fullRows, edgeBlocks, floorBlocks
+     * Minimize (negative weights ideally): totalHeight, holes, blockades, inconsistency
+     */
     double scoreBoard(Board newBoard) {
-        //TODO add more variables??? Ex: edges touching another block, edges touching wall, edges touching floor
-        // get the summed height - each block has value of its height
+
+        // get the summed height - each block has value of its height, minimize
         int totalHeight = 0;
         for (int row = 0; row < newBoard.getHeight(); row++) {
             totalHeight += (row + 1) * newBoard.getRowWidth(row);
         }
 
-        //TODO check if we are using rowsCleared correctly in TetrisBoard
-        //TODO also check if we are catching the fullRows before they are cleared
-        //number of rows that can be cleared, maximize this
+        //number of rows that can be cleared, maximize
         int fullRows = newBoard.getRowsCleared();
-        //System.out.println(fullRows);
 
-        //TODO: implement hole function (empty space such
-        //TODO: that there is at least one tile in column above it)
-        //TODO: Constant time?
-        //number of holes present in the board, minimize this
+        //number of holes and blockades present in the board, minimize
         int holes = 0, blockades = 0;
         for (int x = 0; x < newBoard.getWidth(); x++) {
             int holesInCol = 0;
@@ -214,14 +213,14 @@ public class TacticalBrain implements Brain {
             holes += holesInCol;
         }
 
-        // "bumpiness" of the board, sum of the differences of all adjacent columns
-        // minimize this
+        //sum of the differences of all adjacent columns, minimize
         int inconsistency = 0;
         for (int i = 0; i < newBoard.getWidth()-1; i++) {
             int diff = newBoard.getColumnHeight(i) - newBoard.getColumnHeight(i+1);
             inconsistency = inconsistency + Math.abs(diff);
         }
 
+        //number of blocks bordering the left and right walls of the board
         int edgeBlocks = 0;
         for (int y = 0; y < newBoard.getHeight(); y++)
         {
@@ -230,20 +229,25 @@ public class TacticalBrain implements Brain {
             if (newBoard.getGrid((newBoard.getWidth()-1),y))
                 edgeBlocks++;
         }
+
+        //number of blocks on the bottom of the board
         int floorBlocks = 0;
         for (int x = 0; x < newBoard.getWidth(); x++) {
             if (newBoard.getGrid(x,0))
                 floorBlocks++;
         }
 
+        //scoring function
         double score = weights[0]*totalHeight + weights[1]*fullRows + weights[2]*holes + weights[3]*blockades + weights[4]*inconsistency + weights[5]*edgeBlocks + weights[6]*floorBlocks;
         return score;
     }
 
+    //for testing
     public ArrayList<Board.Action> getFirstMoves() {
         return firstMoves;
     }
 
+    //for testing
     public ArrayList<Board> getOptions() {
         return options;
     }
